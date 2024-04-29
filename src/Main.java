25282cc189641539fb6207eb42f3694812889440
Vladislav Abakumov <vladyslavabakumov@yandex.ru>
2024-04-29 21:19:29 +0800

    init

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import service.AppController;
+
 public class Main {
 
     public static void main(String[] args) {
+        AppController app = new AppController();
         System.out.println("Поехали!");
     }
 }
--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -1,4 +1,11 @@
 package service;
 
+import models.Epic;
+import models.SubTask;
+import models.Task;
+
 public class TaskManager {
+    Epic epic = new Epic();
+    Task task = new Task();
+    SubTask subTask = new SubTask();
 }
