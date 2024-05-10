48a83762e1a4d5cb9870cba8e1df79095644a377
Vladislav Abakumov <vladyslavabakumov@yandex.ru>
2024-05-10 15:51:26 +0800

    create signature and class field

--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -1,11 +1,60 @@
 package service;
 
-import models.Epic;
-import models.SubTask;
-import models.Task;
+import model.Epic;
+import model.SubTask;
+import model.Task;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TaskManager {
-    Epic epic = new Epic();
-    Task task = new Task();
-    SubTask subTask = new SubTask();
+    private HashMap<Integer, Task> tasks;
+    private HashMap<Integer, Epic> epics;
+    private HashMap<Integer, SubTask> subTasks;
+
+    public TaskManager(HashMap<Integer, Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public Task createTask(Task task) {
+        task.setId(task.hashCode());
+        tasks.put(task.getId(), task);
+        return task;
+    }
+
+    public Epic createEpic(Epic task) {
+        task.setId(task.hashCode());
+        tasks.put(task.getId(), task);
+        return task;
+    }
+
+    public SubTask createSubTask(SubTask task) {
+        task.setId(task.hashCode());
+        tasks.put(task.getId(), task);
+        return task;
+    }
+
+    public Task get(int id) {
+        return tasks.get(id);
+    }
+
+    public void update() {
+
+    }
+
+    public void updateEpic() {
+
+    }
+
+    public void updateSubTask() {
+
+    }
+
+    public ArrayList<Task> getAll(){
+        return new ArrayList<>(tasks.values());
+    }
+
+    public void delete(){
+
+    }
 }
