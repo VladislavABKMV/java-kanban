08dd49fcdabc53b6fb94a6ceead395e903cccce8
Vladislav Abakumov <vladyslavabakumov@yandex.ru>
2024-06-11 17:33:09 +0800

    add getHistory method and create private addHistory method

--- src/service/InMemoryTaskManager.java
+++ src/service/InMemoryTaskManager.java
@@ -8,10 +8,11 @@ import model.Task;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class InMemoryTaskManager {
+public class InMemoryTaskManager implements TaskManager {
     private final HashMap<Integer, Task> tasks;
     private final HashMap<Integer, Epic> epics;
     private final HashMap<Integer, SubTask> subTasks;
+    private final ArrayList<Task> history;
 
     private int idCount = 0;
 
@@ -19,29 +20,36 @@ public class InMemoryTaskManager {
         this.tasks = new HashMap<>();
         this.epics = new HashMap<>();
         this.subTasks = new HashMap<>();
+        this.history = new ArrayList<>();
     }
 
+    @Override
     public ArrayList<Task> getAllTask() {
         return new ArrayList<>(tasks.values());
     }
 
+    @Override
     public ArrayList<Epic> getAllEpic() {
         return new ArrayList<>(epics.values());
     }
 
+    @Override
     public ArrayList<SubTask> getAllSubTask() {
         return new ArrayList<>(subTasks.values());
     }
 
+    @Override
     public void deleteAllTask() {
         tasks.clear();
     }
 
+    @Override
     public void deleteAllEpic() {
         epics.clear();
         subTasks.clear();
     }
 
+    @Override
     public void deleteAllSubTask() {
         subTasks.clear();
 
@@ -51,30 +59,36 @@ public class InMemoryTaskManager {
         }
     }
 
+    @Override
     public Task getTask(int id) {
         return tasks.get(id);
     }
 
+    @Override
     public Epic getEpic(int id) {
         return epics.get(id);
     }
 
+    @Override
     public SubTask getSubTask(int id) {
         return subTasks.get(id);
     }
 
+    @Override
     public Task createTask(Task task) {
         task.setId(generateId());
         tasks.put(task.getId(), task);
         return task;
     }
 
+    @Override
     public Epic createEpic(Epic epic) {
         epic.setId(generateId());
         epics.put(epic.getId(), epic);
         return epic;
     }
 
+    @Override
     public SubTask createSubTask(SubTask subTask) {
         Epic epic = epics.get(subTask.getEpicId());
 
@@ -86,10 +100,12 @@ public class InMemoryTaskManager {
         return subTask;
     }
 
+    @Override
     public void updateTask(Task task) {
         tasks.put(task.getId(), task);
     }
 
+    @Override
     public void updateEpic(Epic updateEpic) {
         Epic epic = epics.get(updateEpic.getId());
         if (!epic.equals(updateEpic)) {
@@ -100,6 +116,7 @@ public class InMemoryTaskManager {
         epic.setDescription(updateEpic.getDescription());
     }
 
+    @Override
     public void updateSubTask(SubTask subTask) {
         Epic epic = epics.get(subTask.getEpicId());
 
@@ -107,10 +124,12 @@ public class InMemoryTaskManager {
         updateStatusEpic(epic);
     }
 
+    @Override
     public void deleteTask(int id) {
         tasks.remove(id);
     }
 
+    @Override
     public void deleteEpic(int id) {
        Epic epic = epics.remove(id);
        ArrayList<SubTask> targetEpicSubTasks = getSubTasksForEpic(epic);
@@ -120,6 +139,7 @@ public class InMemoryTaskManager {
         }
     }
 
+    @Override
     public void deleteSubTask(int id) {
         SubTask subTask =  subTasks.remove(id);
         Epic epic = epics.get(subTask.getEpicId());
@@ -128,12 +148,19 @@ public class InMemoryTaskManager {
         updateStatusEpic(epic);
     }
 
+    @Override
     public ArrayList<SubTask> getSubTasksForEpic(Epic epic) {
         ArrayList<SubTask> subTaskForTargetEpic = new ArrayList<>();
         for (int subTaskId: epic.getSubTaskIds()) {
           subTaskForTargetEpic.add(subTasks.get(subTaskId));
         }
         return subTaskForTargetEpic;
+    }
+
+    // доделать
+    @Override
+    public ArrayList<Task> getHistory() {
+        return new ArrayList<>(tasks.values());
     }
 
     private void updateStatusEpic(Epic epic) {
@@ -155,6 +182,10 @@ public class InMemoryTaskManager {
         } else {
             epic.setStatus(Status.IN_PROGRESS);
         }
+    }
+    // доделать
+    private void addHistory(Task task) {
+        history.add(task);
     }
 
     private int generateId() {
