b8f8ea5d5d7542b90ec696af0569f56c8c417ef3
Vladislav Abakumov <vladyslavabakumov@yandex.ru>
2024-06-17 10:43:25 +0800

    create implementation HistoryManager

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -1,4 +1,30 @@
 package service;
 
-public class InMemoryHistoryManager {
+import model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager{
+    private final static int MAX_HISTORY_SIZE = 10;
+    List<Task> history;
+
+    public InMemoryHistoryManager() {
+        this.history = new ArrayList<>();
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return history;
+    }
+
+    @Override
+    public void addHistory(Task task) {
+        if(history.size() < MAX_HISTORY_SIZE) {
+            history.add(task);
+        } else {
+            history.removeFirst();
+            history.add(task);
+        }
+    }
 }
