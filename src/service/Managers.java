187820973738fd27954a0df94a933fa4d9ea4199
Vladislav Abakumov <vladyslavabakumov@yandex.ru>
2024-06-17 10:45:38 +0800

    create static default History method

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -4,4 +4,6 @@ public class Managers {
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
+
+    public static HistoryManager getDefaultHistory() { return new InMemoryHistoryManager(); }
 }
