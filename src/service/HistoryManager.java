b80fd9d9e37f2e057739a9f8574271856563f372
Vladislav Abakumov <vladyslavabakumov@yandex.ru>
2024-06-17 10:27:18 +0800

    create interface

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -1,4 +1,11 @@
 package service;
 
+import model.Task;
+
+import java.util.List;
+
 public interface HistoryManager {
+    public List<Task> getHistory();
+
+    public void addHistory(Task task);
 }
