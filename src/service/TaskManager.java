2b70b5d40f32c6af7854d3bb59af9334c8165c63
Vladislav Abakumov <vladyslavabakumov@yandex.ru>
2024-06-25 19:28:48 +0800

    delete all public

--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -8,43 +8,43 @@ import java.util.List;
 
 public interface TaskManager {
 
-    public List<Task> getAllTask();
+    List<Task> getAllTask();
 
-    public List<Epic> getAllEpic();
+    List<Epic> getAllEpic();
 
-    public List<SubTask> getAllSubTask();
+    List<SubTask> getAllSubTask();
 
-    public void deleteAllTask();
+    void deleteAllTask();
 
-    public void deleteAllEpic();
+    void deleteAllEpic();
 
-    public void deleteAllSubTask();
+    void deleteAllSubTask();
 
-    public Task getTask(int id);
+    Task getTask(int id);
 
-    public Epic getEpic(int id);
+    Epic getEpic(int id);
 
-    public SubTask getSubTask(int id);
+    SubTask getSubTask(int id);
 
-    public Task createTask(Task task);
+    Task createTask(Task task);
 
-    public Epic createEpic(Epic epic);
+    Epic createEpic(Epic epic);
 
-    public SubTask createSubTask(SubTask subTask);
+    SubTask createSubTask(SubTask subTask);
 
-    public void updateTask(Task task);
+    void updateTask(Task task);
 
-    public void updateEpic(Epic updateEpic);
+    void updateEpic(Epic updateEpic);
 
-    public void updateSubTask(SubTask subTask);
+    void updateSubTask(SubTask subTask);
 
-    public void deleteTask(int id);
+    void deleteTask(int id);
 
-    public void deleteEpic(int id);
+    void deleteEpic(int id);
 
-    public void deleteSubTask(int id);
+    void deleteSubTask(int id);
 
-    public List<SubTask> getSubTasksForEpic(Epic epic);
+    List<SubTask> getSubTasksForEpic(Epic epic);
 
-    public List<Task> getHistory();
+    List<Task> getHistory();
 }
