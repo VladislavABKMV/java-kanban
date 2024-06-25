a6b3f8e7148ae751ed21af67f7463696dc00f843
Vladislav Abakumov <vladyslavabakumov@yandex.ru>
2024-06-25 19:27:52 +0800

    create all tests for SubTask

---
+++ test/model/SubTaskTest.java
@@ -0,0 +1,28 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Тестирование класса SubTask")
+class SubTaskTest {
+    @Test
+    @DisplayName("Тест сравнение подзадачи со своей копией")
+    public void shouldEqualsWithCopy() {
+        SubTask subTask = new SubTask(1, "name", Status.NEW, "description", 0);
+        SubTask copySubTask = new SubTask(subTask.getId(), "name", Status.NEW,
+                "description", 0);
+
+        assertSubTaskEquals(subTask, copySubTask);
+    }
+
+    private static void assertSubTaskEquals(SubTask subTask, SubTask copySubTask) {
+        assertEquals(subTask.getId(), copySubTask.getId(), "id " + "должны совпадать.");
+        assertEquals(subTask.getName(), copySubTask.getName(), "name  " + "должны совпадать.");
+        assertEquals(subTask.getStatus(), copySubTask.getStatus(), "status " + "должны совпадать.");
+        assertEquals(subTask.getDescription(), copySubTask.getDescription(),
+                "description " + "должны совпадать.");
+        assertEquals(subTask.getEpicId(), copySubTask.getEpicId(), "epicId " + "должны совпадать.");
+    }
+}
