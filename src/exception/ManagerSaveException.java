784844967f4405e4c74389ddaab871c6df11201a
Vladislav Abakumov <vladyslavabakumov@yandex.ru>
2024-08-12 17:50:36 +0800

    init

---
+++ src/exception/ManagerSaveException.java
@@ -0,0 +1,9 @@
+package exception;
+
+import java.io.IOException;
+
+public class ManagerSaveException extends IOException {
+    public ManagerSaveException(String message) {
+        super(message);
+    }
+}
